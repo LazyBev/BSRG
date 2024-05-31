f28bee85bf1401dd73565b002be8e328f2f5de64
Lazy Beverage <157259616+LazyBev@users.noreply.github.com>
2024-05-31 10:49:55 +0100

    Update xRan.c

--- xRan.c
+++ xRan.c
@@ -1,12 +1,29 @@
 #include "xRan.h"
 
 uint32_t state = 1;
+int fs, fa, fc;
+
+int mod(int a, int b) {
+    return (a % b - b) % b;
+}
+
+void saveInFile(char *fName, int n, uint32_t seed,uint32_t s, uint32_t a, uint32_t c) {
+    FILE *file = fopen(fName, "w+");
+    seed_bsrg(seed, s, a, c);
+    for (int i = 0; i < n; i++) {
+        fprintf(file, "%d\n", bsrg(i, true));
+    }
+    fclose(file);
+}
 
 uint32_t seed_bsrg(uint32_t seed, uint32_t s, uint32_t a, uint32_t c) {
+    fs, fa, fc = s, a, c;
     state = seed;
     state ^= state << s;
-    state ^= ((s % 2) == 0) ? (state >> (s + 4)) : (state >> (s - 4));
-    state ^= (((state - s) % 2) == 0) ? (state >> ((s / 2) + s)) : (state >> ((s - 10) - 2));
+    state ^= ((s % 2) == 0) ? (state >> (s + seed)) : (state >> (s - seed));
+    state ^= ((s % 2) == 0) ? (state >> ((s - (seed + 10)) - (seed / 2))) : (state >> ((s - (seed + 10)) - (seed / 2)));
+    if ((s % 2) == 0)
+        state = (state < 0) ? -state : state;
     state = (a * (state + c)) / (a + c);
     return state;
 }
@@ -16,8 +33,14 @@ uint32_t bsrg(int E, bool allow) {
     if (!allow) {
         newState = (newState < 0) ? -newState : newState;
     }
-    int B = newState * E;
-    newState = (B - (B / (B * ((B + E) - E) / E))) + (B / (B * E - B));
+
+    int B = newState + (mod(E, newState) - E) * (mod(E, newState) + E);
+    for(int i = 0; i < 4; ++i) {
+        int C = -B;
+        int D = mod(E, B) * mod(E, C);
+        B = newState + (mod(E, newState) - E) * (mod(E, newState) + E) * D;
+        newState = state + ((B * fs) - (fa *(B * (B * ((B - E) - E)) + E))) + (fc + (B + (B * E - B)));
+    }
     state = newState;
     return newState;
 }
