8f561588d4d3af4ee2227f9db8a4950a8e96a1fb
Lazy Beverage <157259616+LazyBev@users.noreply.github.com>
2024-05-31 10:50:16 +0100

    Update xRan.h

--- xRan.h
+++ xRan.h
@@ -1,5 +1,7 @@
 #pragma once
 
+#include <stdio.h>
+
 #ifndef XRAN_H
 #define XRAN_H
 
@@ -15,8 +17,11 @@ typedef enum {
     true = 1
 } bool;
 
+int mod(int a, int b);
+
 extern uint32_t state;
 
+void saveInFile(char *name, int n, uint32_t seed,uint32_t s, uint32_t a, uint32_t c);
 uint32_t seed_bsrg(uint32_t seed, uint32_t s, uint32_t a, uint32_t c);
 uint32_t bsrg(int E, bool allow);
 
