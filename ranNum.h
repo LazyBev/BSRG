eabed64e09d6b76be231ab8d81cdce6ce9be634a
Lazy Beverage <157259616+LazyBev@users.noreply.github.com>
2024-05-31 01:23:37 +0100

    Update ranNum.h

--- ranNum.h
+++ ranNum.h
@@ -1,9 +1,10 @@
 #include <math.h>
 #include <stdio.h>
+#include <stdbool.h>
 #include <stdlib.h>
 #include <sys/random.h>
 #include <time.h>
 #include <unistd.h>
 
 void sera(int e);
-int ranu(int u);
+int ranu(int u, bool allow);
