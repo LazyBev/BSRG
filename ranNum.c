df5fd0685a8768e124990c2c86f32229acff9a8d
Lazy Beverage <157259616+LazyBev@users.noreply.github.com>
2024-05-31 01:23:10 +0100

    Update ranNum.c

--- ranNum.c
+++ ranNum.c
@@ -2,12 +2,13 @@
 
 void sera(int e) { srand(e * (time(NULL) * getpid())); }
 
-int ranu(int u) {
+int ranu(int u, bool allow) {
   sera(7);
   float B = rand() * u;
   int A = B + floor((B / 10000000) / getpid());
 
-  A = (A < 0) ? -A : A;
+  if (!allow)
+    A = (A < 0) ? -A : A;
 
   return A;
 }
