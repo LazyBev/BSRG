13f743bf7fa5cba271e566d07919012f8d2eaade
Lazy Beverage <157259616+LazyBev@users.noreply.github.com>
2024-05-31 02:27:40 +0100

    Update ranu.c

--- ranu.c
+++ ranu.c
@@ -1,18 +1,17 @@
 #include "ranu.h"
 
 // "int e" (e for entropy) adds entropy to the generation of the number 
-void sera(int e) { srand(e * (time(NULL) * getpid())); }
+void sera(int E) { srand(E * (time(NULL) * getpid())); }
 
 // "int u" is a modifer to add more entropy to the generation of the number 
 // "bool allow" is a boolean that decides if negative numbers can be produced or not (true = possiblity of negatives) 
-int ranu(int U, bool allow) {
-  sera(U);
-  float B = rand() * U;
-  int A = B + ((B - (B / (B * (U * U)))) / (B / getpid()));
+int ranu(int E, bool allow) {
+  sera(E);
+  float B = rand() * E;
+  int A = B + ((B - (B / (B * (E * E)))) / (B / getpid()));
 
   if (!allow)
     A = (A < 0) ? -A : A;
 
-  printf("B = %f\n", B);
   return A;
 }
