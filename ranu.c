89ccf05cf5402190115f5dc2d246b574066edb43
Lazy Beverage <157259616+LazyBev@users.noreply.github.com>
2024-05-31 01:34:26 +0100

    Update ranu.c

--- ranu.c
+++ ranu.c
@@ -1,9 +1,12 @@
 #include "ranu.h"
 
 // Seeds Random Number Generator
+// "int e" is a modifer to add more entropy to the generation of the number 
 void sera(int e) { srand(e * (time(NULL) * getpid())); }
 
 // Generates Random Number
+// "int u" is a modifer to add more entropy to the generation of the number 
+// "bool allow" is a boolean that decides if negative numbers can be produced or not (true = possiblity of negatives) 
 int ranu(int u, bool allow) {
   sera(7);
   float B = rand() * u;
