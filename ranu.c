38d77593e047235b007fbe438d50678927468f01
Lazy Beverage <157259616+LazyBev@users.noreply.github.com>
2024-05-31 02:03:46 +0100

    Update ranu.c

--- ranu.c
+++ ranu.c
@@ -7,10 +7,10 @@ void sera(int e) { srand(e * (time(NULL) * getpid())); }
 // Generates Random Number
 // "int u" is a modifer to add more entropy to the generation of the number 
 // "bool allow" is a boolean that decides if negative numbers can be produced or not (true = possiblity of negatives) 
-int ranu(int u, bool allow) {
+int ranu(int U, bool allow) {
   sera(7);
-  float B = rand() * u;
-  int A = B + floor((B - (B / 10000000)) / (B / getpid()));
+  float B = rand() * U;
+  int A = B + floor((B - (B / (B * (U * U)))) / (B / getpid()));
 
   if (!allow)
     A = (A < 0) ? -A : A;
