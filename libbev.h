059e2db58b7a0beb646f98105784a2d0ab2ea1cb
Lazy Beverage <157259616+LazyBev@users.noreply.github.com>
2024-06-12 17:09:03 +0000

    Update libbev.h

--- libbev.h
+++ libbev.h
@@ -15,6 +15,27 @@ typedef unsigned int uint32_t;
 typedef unsigned short uint16_t;
 typedef unsigned char uint8_t;
 
+// Function initialization
+uint64_t NOT(uint64_t in);
+uint64_t NAND(uint64_t in1, uint64_t in2);
+uint64_t NOR(uint64_t in1, uint64_t in2);
+uint64_t XNOR(uint64_t in1, uint64_t in2);
+uint64_t AND(uint64_t in1, uint64_t in2);
+uint64_t OR(uint64_t in1, uint64_t in2);
+uint64_t XOR(uint64_t in1, uint64_t in2);
+
+void halfAdder(int x, int y, int *sum, int *car);
+void fullAdder(int c, int x, int y, int *sum, int *car);
+int byteAdder(int in1, int in2);
+
+double expo(double x, double e);
+double squ(double x);
+double squrot(double t);
+int abso(double x);
+
+uint64_t Bsrg(uint64_t seed, uint64_t s, uint64_t a, uint64_t c, uint64_t E, char* allow);
+uint64_t wait(uint64_t clockSpeed, uint64_t milliseconds);
+
 
 // Logic gates
 uint64_t NOT(uint64_t in) {
@@ -108,46 +129,53 @@ int abso(double x) {
 }
 
 // Radomizer
-uint64_t Bsrg(uint64_t seed, uint64_t s, uint64_t a, uint64_t c, uint64_t E, bool allow) {
+uint64_t Bsrg(uint64_t seed, uint64_t s, uint64_t a, uint64_t c, uint64_t E, char* allow) {
     uint64_t state = seed;
     uint64_t newState = state;
-    uint64_t fs, fa, fc;
+    uint64_t fs = s, fa = a, fc = c;
 
-    // Seed
-    fs = s;
-    fa = a;
-    fc = c;
     state ^= state << s;
     state ^= (s % 2 == 0) ? (state >> (s + seed)) : (state >> (s - seed));
-    state ^= (s % 2 == 0) ? (state >> ((s - (seed + 10)) - (seed / 2))) : (state >> ((s - (seed + 10)) - (seed / 2)));
+    state ^= state >> ((s - (seed + 10)) - (seed / 2));
     state = (a * (state + c)) / (a + c);
 
-    // Randomize
-    newState = state;
-    if (!allow) {
-        newState = (newState < 0) ? -newState : newState;
+    if (allow && newState < 0) {
+        newState = -newState;
+    } else if(allow && newState < 0) {
+
     }
 
-    uint64_t B = (state % newState == 0) ? newState + ((E % newState) - E) * ((E % newState) + E) 
-                                          : newState + ((E % newState) + (E + (E - 2))) * ((E % newState) - (E * E));
+    uint64_t B = (state % newState == 0) 
+                 ? newState + ((E % newState) - E) * ((E % newState) + E)
+                 : newState + ((E % newState) + (E + (E - 2))) * ((E % newState) - (E * E));
+
     for (uint64_t i = 0; i < 4; ++i) {
         uint64_t D = (E % B) - (E % -B);
         B = newState + ((B % newState) - E) * ((E % newState) + E) * D;
-        newState = state + ((B * (fs - B)) - ((fa - B) * (B * (B * ((B - E) - E)) + E))) + ((fc - B) + (B + (B * E - B)));
+        newState = state + ((B * (fs - B)) - ((fa - B) * (B * (B * ((B - E) - E)) + E))) 
+                          + ((fc - B) + (B + (B * E - B)));
     }
+
     state = newState;
 
-    newState = (newState <= 999999999999999999) ? (newState * (newState / 10000000000000000) * 10) : newState;
-    newState = (newState >= 99999999999999999) ? (newState / (newState / 10000000000000000)) : newState;
+    if (newState <= 999999999999999999) {
+        newState = newState * (newState / 10000000000000000) * 10;
+    } else if (newState >= 99999999999999999) {
+        newState = newState / (newState / 10000000000000000);
+    }
 
     return newState;
 }
 
 // Approximation of a wait
 uint64_t wait(uint64_t clockSpeed, uint64_t milliseconds) {
-    clockSpeed *= 1000000000;
-    for (uint64_t i = 0; i < (milliseconds * clockSpeed * 1000); ++i) {
+    uint64_t ns_per_tick = 1000000000 * clockSpeed;
+    uint64_t ticks = (milliseconds * 1000000) / ns_per_tick;
+
+    for (uint64_t i = 0; i < ticks; ++i) {
+        __asm__ __volatile__("");
     }
+
     return 0;
 }
 
